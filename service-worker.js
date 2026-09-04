b3b66b222bf9ac2b88b42592fd5fb3dd54d09fb9
belshie1 <belshie1@gmail.com>
2026-09-04 21:33:42 +0200

    Deploy Pipewise with GitHub Actions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pipewise-v2';
+const CACHE_NAME = 'pipewise-v3';
 
 const APP_FILES = [
     './',
@@ -20,13 +20,13 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys()
-            .then(cacheNames =>
-                Promise.all(
+            .then(cacheNames => {
+                return Promise.all(
                     cacheNames
                         .filter(cacheName => cacheName !== CACHE_NAME)
                         .map(cacheName => caches.delete(cacheName))
-                )
-            )
+                );
+            })
             .then(() => self.clients.claim())
     );
 });
@@ -55,11 +55,17 @@ self.addEventListener('fetch', event => {
 
                         return caches.open(CACHE_NAME)
                             .then(cache => {
-                                cache.put(event.request, networkResponse.clone());
+                                cache.put(
+                                    event.request,
+                                    networkResponse.clone()
+                                );
+
                                 return networkResponse;
                             });
                     })
-                    .catch(() => caches.match('./index.html'));
+                    .catch(() => {
+                        return caches.match('./index.html');
+                    });
             })
     );
 });
